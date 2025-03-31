625108218428671f9f5bcf172f509dafacd20607
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-03-31 15:49:56 +0700

    Refactor deals data structure to use TypeScript types and update asset paths for consistency

--- public/script.ts
+++ public/script.ts
@@ -1,54 +1,68 @@
-export const deals = [
+type CardDeals = {
+  title: string;
+  image: string;
+  price: string;
+  icon_area: string;
+  icon_wifi: string;
+  icon_person: string;
+  icon_star: string;
+  ukuran_area: number;
+  max_person: number;
+  max_wifi: number;
+  rates: string;
+};
+
+export const deals: CardDeals[] = [
   {
     title: "De Flecce",
-    image: "./assets/card-1.png",
+    image: "/assets/card-1.png",
     price: "$47,298",
-    icon_area: "./assets/icon/format-square.svg",
-    icon_wifi: "./assets/icon/wifi.svg",
-    icon_person: "./assets/icon/profile-user-white.svg",
-    icon_star: "./assets/icon/Star.svg",
-    ukuran_area: "529",
-    max_person: "3",
-    max_wifi: "10",
+    icon_area: "/assets/icon/format-square.svg",
+    icon_wifi: "/assets/icon/wifi.svg",
+    icon_person: "/assets/icon/profile-user-white.svg",
+    icon_star: "/assets/icon/Star.svg",
+    ukuran_area: 529,
+    max_person: 3,
+    max_wifi: 10,
     rates: "4.5/5"
   },
   {
     title: "Party Every",
-    image: "./assets/card-2.png",
+    image: "/assets/card-2.png",
     price: "$18,983",
-    icon_area: "./assets/icon/format-square.svg",
-    icon_wifi: "./assets/icon/wifi.svg",
-    icon_person: "./assets/icon/profile-user-white.svg",
-    icon_star: "./assets/icon/Star.svg",
-    ukuran_area: "529",
-    max_person: "3",
-    max_wifi: "10",
+    icon_area: "/assets/icon/format-square.svg",
+    icon_wifi: "/assets/icon/wifi.svg",
+    icon_person: "/assets/icon/profile-user-white.svg",
+    icon_star: "/assets/icon/Star.svg",
+    ukuran_area: 529,
+    max_person: 3,
+    max_wifi: 10,
     rates: "4.5/5"
   },
   {
     title: "Brown Hall",
-    image: "./assets/card-3.png",
+    image: "/assets/card-3.png",
     price: "$84,209",
-    icon_area: "./assets/icon/format-square.svg",
-    icon_wifi: "./assets/icon/wifi.svg",
-    icon_person: "./assets/icon/profile-user-white.svg",
-    icon_star: "./assets/icon/Star.svg",
-    ukuran_area: "529",
-    max_person: "3",
-    max_wifi: "10",
+    icon_area: "/assets/icon/format-square.svg",
+    icon_wifi: "/assets/icon/wifi.svg",
+    icon_person: "/assets/icon/profile-user-white.svg",
+    icon_star: "/assets/icon/Star.svg",
+    ukuran_area: 529,
+    max_person: 3,
+    max_wifi: 10,
     rates: "4.5/5"
   },
   {
     title: "Mini Max",
-    image: "./assets/card-4.png",
+    image: "/assets/card-4.png",
     price: "$14,221",
-    icon_area: "./assets/icon/format-square.svg",
-    icon_wifi: "./assets/icon/wifi.svg",
-    icon_person: "./assets/icon/profile-user-white.svg",
-    icon_star: "./assets/icon/Star.svg",
-    ukuran_area: "529",
-    max_person: "3",
-    max_wifi: "10",
+    icon_area: "/assets/icon/format-square.svg",
+    icon_wifi: "/assets/icon/wifi.svg",
+    icon_person: "/assets/icon/profile-user-white.svg",
+    icon_star: "/assets/icon/Star.svg",
+    ukuran_area: 529,
+    max_person: 3,
+    max_wifi: 10,
     rates: "4.5/5"
   }
 ];
